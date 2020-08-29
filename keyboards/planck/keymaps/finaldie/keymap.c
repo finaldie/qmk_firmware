87da42461a083b3532f3b95c4bbd29919e6d92c6
Yuzhang Hu <hyzwowtools@gmail.com>
2020-08-28 21:03:00 -0700

    FL1: adjust comma and dot

--- keyboards/planck/keymaps/finaldie/keymap.c
+++ keyboards/planck/keymaps/finaldie/keymap.c
@@ -123,7 +123,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  */
 [_GAME] = LAYOUT_planck_grid(
     KC_TAB,  KC_HOME, KC_UP,   KC_END,  KC_F1,   KC_F2,   KC_F3,   KC_7,    KC_8,    KC_9,    KC_PLUS, KC_BSPC,
-    _______, KC_DOT,  KC_COMM, KC_RIGHT,KC_F4,   KC_F5,   KC_F6,   KC_4,    KC_5,    KC_6,    KC_MINS, _______,
+    _______, KC_DOT,  KC_COMM, KC_COMM, KC_F4,   KC_F5,   KC_F6,   KC_4,    KC_5,    KC_6,    KC_MINS, _______,
     _______, KC_ASTR, KC_DOT,  KC_COMM, KC_F7,   KC_F8,   KC_F9,   KC_1,    KC_2,    KC_3,    _______, _______,
     _______, _______, _______, _______, LT_MINS, KC_SPC,  KC_SPC,  LT_EQL,  KC_0,    KC_DOT,  KC_ASTR, _______
 ),
